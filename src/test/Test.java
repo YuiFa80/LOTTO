3bcdb446b014e7aac851c8ba9367e8f914196370
ky269 <ky269@HanJH>
2021-12-19 22:59:21 +0900

    lotto_test

---
+++ src/test/Test.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+
+public class Test {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int[] lottoNum = new int[6];
+		System.out.println("제외 값은 몇개인가요? 없으면 0으로 해주세요");
+		int n = sc.nextInt();
+		int[] exceptionNum = new int[n];
+		System.out.println("제외할로또값을 입력하시오");
+		if (n != 0) {
+			for (int i = 0; i < exceptionNum.length; i++) {
+				System.out.print("제외값은:");
+				exceptionNum[i] = sc.nextInt();
+			}
+		}
+
+		System.out.println("몇장을 살것인가요??");
+		int q = sc.nextInt();
+		int[] han = new int[q];
+		for (int w = 0; w < han.length; w++) {
+			for (int i = 0; i < lottoNum.length; i++) {
+
+				lottoNum[i] = (int) (Math.random() * 45 + 1);
+				for (int j = 0; j < exceptionNum.length; j++) {
+
+					if (lottoNum[i] == exceptionNum[j]) {
+						i--;
+						break;
+					}
+
+				}
+				for (int k = 0; k < lottoNum.length; k++) {
+					if (i != k && lottoNum[i] == lottoNum[k]) {
+						i--;
+						break;
+					}
+				}
+			}
+			
+			
+		
+			System.out.println("구매번호 :" + Arrays.toString(lottoNum));
+
+		}
+	}
+
+}
+	
+
